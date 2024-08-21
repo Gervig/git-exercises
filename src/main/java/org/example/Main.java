2b1a00669cb39ac0b6f4f81e9406bc9949023da2
RHoejholt <158362000+RHoejholt@users.noreply.github.com>
2024-08-21 15:43:13 +0200

    100 ->> 200

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
 
 public class Main {
-    private static void FizzBuzz() {
-        for (int i = 1; i <= 100; i++) {
+    private static void fizzBuzz() {
+        for (int i = 1; i <= 200; i++) {
             System.out.println(i);
             if ((i % 3) == 0 && (i % 5) == 0) {
                 System.out.print("FizzBuzz\n");
