f0258a47835e8f558c659ae849767afe1421a207
Gervig <gervig91@gmail.com>
2024-08-21 15:02:52 +0200

    Casper FizzBuzz

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,23 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        System.out.println("hejsa");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void fizzBuzz(){
+        for(int i = 1; i < 100; i++){
+            String res = "";
+            if(i%3==0){
+                res += "Fizz";
+            }
+            if(i%5==0){
+                res += "Buzz";
+            }
+            if(i%3!=0 && i%5!=0){
+                res += "" + i;
+            }
+            System.out.println(res);
         }
+}
+    public static void main(String[] args) {
+        fizzBuzz();
     }
+
 }
